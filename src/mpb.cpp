dd85331b1e062c781644b941660822d485877871
Kumar Harsha <kumar.harsha@tum.de>
2017-03-21 11:37:38 +0530

    optimise qmpb for single parameters

--- src/mpb.cpp
+++ src/mpb.cpp
@@ -4,6 +4,7 @@
 #include <gsl/gsl_sf_hyperg.h>
 #include <Rcpp.h>
 #include <cmath>
+#include <cstdlib>
 #include "mpb.h"
 #include "shared.h"
 using namespace Rcpp;
@@ -55,7 +56,7 @@ double pmpb_(double x, double alpha, double beta, double c) {
 
 // distribution function array
 double* pmpb_(double alpha, double beta, double c) {
-  double res[Q_LIMIT];
+  double *res = (double *)std::malloc(Q_LIMIT * sizeof(double));
   res[0] = dmpb_(0, alpha, beta, c);
   for(int i = 1; i < Q_LIMIT; i++) {
     res[i] = res[i-1] + dmpb_(i, alpha, beta, c);
@@ -63,7 +64,24 @@ double* pmpb_(double alpha, double beta, double c) {
   return res;
 }
 
-// quantiles
+// quantiles for single parameters
+int qmpb_(double p, double *p_distr) {
+  int i;
+  if(p > p_distr[Q_LIMIT-1]) {
+    return Q_LIMIT;
+  }
+  if(p < p_distr[0]) {
+    return 0;
+  }
+  for(i = 1; i < Q_LIMIT; i++) {
+    if(p > p_distr[i-1] && p < p_distr[i]) {
+      return i;
+    }
+  }
+  return i;
+}
+
+// quantiles for vectorised parameters
 int qmpb_(double p, double alpha, double beta, double c) {
   if(isInadmissible(p) || !validProbability(p))
     return 0;
@@ -191,11 +209,12 @@ NumericVector qmpb(NumericVector p, NumericVector alpha, NumericVector beta, Num
 
     if (1 == alpha.size() && 1 == beta.size() && 1 == c.size()) {
       // single parameters
+      double* p_distr = pmpb_(alpha[0], beta[0], c[0]);
       for(int i = 0; i < n; i++) {
         if(p[i] == 1.0) {
           res[i] = R_PosInf;
         } else {
-          res[i] = qmpb_(p[i], alpha[0], beta[0], c[0]);
+          res[i] = qmpb_(p[i], p_distr);
           res[i] = res[i] == Q_LIMIT ? R_PosInf : res[i];
         }
       }
