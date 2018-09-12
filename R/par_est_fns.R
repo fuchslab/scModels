5279b2332537547e3c519afe732fbdbc8a74419c
Kumar Harsha <kumar.harsha@tum.de>
2018-09-12 13:26:42 +0530

    bug fix in pb initial estimate

--- R/par_est_fns.R
+++ R/par_est_fns.R
@@ -142,12 +142,13 @@ estimate_pb_optim_init <- function(x, iter = 200) {
     x1 <- r1 * r2 - 2 * r1 * r3 + r2 * r3
     x2 <- r1 - 2 * r2 + r3
     alpha <- 2 * r1 * (r3 - r2) / x1
-    if (alpha > 0)
-      sampled_params <- c(sampled_params, alpha)
+    if(alpha < 0)
+      alpha <- runif(1)
+    cm <- c(alpha, 0, max(d1))
+    cm[2] <- (function(a, c, m) a * c / m - a)(cm[1], cm[3], mean(d1))
+    sampled_params <- rbind(sampled_params, cm)
   }
-  cm <- c(mean(sampled_params), 0, max(x))
-  cm[2] <- (function(a, c, m) a * c / m - a)(cm[1], cm[3], mean(x))
-  return(cm)
+  return(colMeans(sampled_params))
 }
 
 
