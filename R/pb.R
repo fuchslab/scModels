02f734745f256c4ccfa9cee04e168f31caa61693
Amrhein <lisa.amrhein@helmholtz-muenchen.de>
2019-06-03 17:54:26 +0200

    Last Build changes

--- R/pb.R
+++ R/pb.R
@@ -27,7 +27,7 @@
 #'  plot(0:200, X, type='l')
 #'  Y <- dpb(0:10, seq(10.0,11.0,by=0.1), seq(30.0,31.0,by=0.1), seq(10.2,11.2,by=0.1))
 dpb <- function(x, alpha, beta, c = 1, log = FALSE) {
-  cpp_dpb(x, alpha, beta, c, log)
+    cpp_dpb(x, alpha, beta, c, log)
 }
 
 
@@ -37,7 +37,7 @@ dpb <- function(x, alpha, beta, c = 1, log = FALSE) {
 #'  Y <- ppb(q= 0 :200, alpha=5, beta= 3, c=20)
 #'  plot(0:200, Y, type="l")
 ppb <- function(q, alpha, beta, c = 1, lower.tail = TRUE, log.p = FALSE) {
-  cpp_ppb(q, alpha, beta, c, lower.tail, log.p)
+    cpp_ppb(q, alpha, beta, c, lower.tail, log.p)
 }
 
 
@@ -47,7 +47,7 @@ ppb <- function(q, alpha, beta, c = 1, lower.tail = TRUE, log.p = FALSE) {
 #'  Z <- qpb(p= seq(0,1, by= 0.01), alpha=5, beta= 3, c=20)
 #'  plot(seq(0,1, by= 0.01),Z, type="l")
 qpb <- function(p, alpha, beta, c = 1, lower.tail = TRUE, log.p = FALSE) {
-  cpp_qpb(p, alpha, beta, c, lower.tail, log.p)
+    cpp_qpb(p, alpha, beta, c, lower.tail, log.p)
 }
 
 
@@ -59,5 +59,5 @@ qpb <- function(p, alpha, beta, c = 1, lower.tail = TRUE, log.p = FALSE) {
 #'  lines(density(RV), col="red")
 #'  R2 <- rpb(11, seq(10.0,11.0,by=0.1), seq(30.0,31.0,by=0.1), seq(10.2,11.2,by=0.1))
 rpb <- function(n, alpha, beta, c = 1) {
-  cpp_rpb(n, alpha, beta, c)
+    cpp_rpb(n, alpha, beta, c)
 }
