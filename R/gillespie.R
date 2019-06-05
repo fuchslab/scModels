666341d7cc47c71111647f98bb9988345990e985
Kumar Harsha <kumar.harsha@tum.de>
2019-06-06 00:46:18 +0200

    compile examples

--- R/gillespie.R
+++ R/gillespie.R
@@ -18,7 +18,7 @@
 #' @name gmRNA
 #' @rdname gmRNA
 #' @export
-#' @example
+#' @examples
 #' x <- gmRNA_basic(100, 0.75, 0.001)
 #' plot(density(x))
 gmRNA_basic <- function(n, r.on, r.degr) {
@@ -28,7 +28,7 @@ gmRNA_basic <- function(n, r.on, r.degr) {
 
 #' @rdname gmRNA
 #' @export
-#' @example
+#' @examples
 #' x <- gmRNA_switch(100, 0.23, 0.15, 0.75, 0.001)
 #' plot(density(x))
 gmRNA_switch <- function(n, r.act, r.deact, r.on, r.degr) {
@@ -38,7 +38,7 @@ gmRNA_switch <- function(n, r.act, r.deact, r.on, r.degr) {
 
 #' @rdname gmRNA
 #' @export
-#' @example
+#' @examples
 #' x <- gmRNA_burst(10, 0.15, 0.75, 0.001)
 #' plot(density(x))
 gmRNA_burst <- function(n, r.burst, s.burst, r.degr) {
--- R/par_est_fns.R
+++ R/par_est_fns.R
@@ -17,7 +17,7 @@
 #' p1 <- fit_params(x1, "nb")
 #' s <- sample(x = c(0,1), size = 100, replace = TRUE, prob = c(0.3,0.7))
 #' x2 <- s*x1 + (1-s)*rnbinom(100, size = 15, mu = 53)
-#' p2 <- fit_params(x2, "nb2)
+#' p2 <- fit_params(x2, "nb2")
 fit_params <- function(x, type, optim_control = list(maxit = 1000)) {
   max_iter <- 20
   x_mean = mean(x)
